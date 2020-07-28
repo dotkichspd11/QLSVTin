a78c5c5455601aa33462951ae0818d00d2a2d435
dotkichspd11 <hiimmax99@gmail.com>
2020-07-28 21:10:15 +0700

    QLSV Tin

---
+++ header.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+	error_reporting(0);
+	$url = "http://localhost:8080/QLSVTin/";
+ ?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <title><?php echo $title;?>Trường Đại Học Sư Phạm Đà Nẵng</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?php echo $url;?>css/reset.css">
+  <link rel="stylesheet" href="<?php echo $url;?>css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $url;?>css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="<?php echo $url;?>css/style.css">
+  <script src="<?php echo $url;?>js/jquery-3.1.0.min.js"></script>
+  <script src="<?php echo $url;?>js/bootstrap.min.js"></script>
+  <script src="<?php echo $url;?>js/style.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo $url;?>images/logo.jpg" />
+</head>
+<body>
+<div class="container">
+	<!-- Header -->
+	<header >
+		<a href="index.php" title="Trường Đại Học Sư Phạm Đà Nẵng">
+			<p>Trường đại học sư phạm Đà Nẵng</p>
+			<p>Da Nang University of Education</p>
+		</a>
+	</header>
+	<!-- End header -->
+</div>
